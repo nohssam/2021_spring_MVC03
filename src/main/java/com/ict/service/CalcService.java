f01328a45eb07c022aa0e0ce2a262351a149ff09
nohssam <nohssam73@gmail.com>
2021-07-09 12:57:18 +0900

    first

---
+++ src/main/java/com/ict/service/CalcService.java
@@ -0,0 +1,22 @@
+package com.ict.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+// Service : DB 트랜잭션처리, DB결과를 가공
+// DAO     : DB 결과 받기 
+@Service("calc")
+public class CalcService {
+	public int plus() {
+		return 5+3 ;
+	}
+	public int minus() {
+		return 5-3 ;
+	}
+	public int multi() {
+		return 5*3 ;
+	}
+	public int divide() {
+		return 5/3 ;
+	}
+}
